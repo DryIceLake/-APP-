29bb55a71dca15438a88ec834573d40e78e71aaa
DryIceLake <2352719981@qq.com>
2019-07-26 10:16:14 +0800

    change

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="wrapper">
     <swiper :options="swiperOption">
-      <swiper-slide><img src="https://image2.suning.cn/uimg/aps/material/155058120028429292.jpg"></swiper-slide>
-      <swiper-slide><img src="https://image3.suning.cn/uimg/cms/img/156395130619847894.jpg"></swiper-slide>
-      <swiper-slide><img src="https://image2.suning.cn/uimg/aps/material/155058120060465377.jpg"></swiper-slide>
+      <swiper-slide v-for="item of swiperList" :key="item.id"><img class="swiper-img" :src="item.imgUrl"></swiper-slide>
     <div class="swiper-pagination"  slot="pagination"></div>
   </swiper>
   </div>
@@ -13,17 +11,40 @@ export default {
   name: 'HomeSwiper',
   data () {
     return {
-      swiperOption: {}
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true,
+        autoplay: 2000
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'https://image2.suning.cn/uimg/aps/material/155058120028429292.jpg'
+      }, {
+        id: '0002',
+        imgUrl: "https://image1.suning.cn/uimg/aps/material/156371100357707416.jpg"
+      }, {
+        id: '0003',
+        imgUrl: 'https://image2.suning.cn/uimg/aps/material/155058120060465377.jpg'
+      }]
     }
   }
 }
 </script>
 <style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: rgba(255,255,255,0.5) !important
+    width: 0.09rem !important
+  .wrapper >>> .swiper-pagination-bullet
+    border-radius: 0
+    width: 0.05rem
+    height: 0.03rem
   .wrapper
     width: 100%
     height: 0
     overflow: hidden
     padding-bottom: 50%
+    background: #eee
   .swiper-img
     width: 100%
+    height: 100%
 </style>
